0ceacf5502c542eaee16294c36862a6976288d3b
CROIRE-Aline <niyocroirealine@gmail.com>
2024-03-09 09:36:58 +0200

    This is a task schedular

---
+++ index.js
@@ -0,0 +1,54 @@
+const tasks=[];
+//fuction to add tasks on scheduler
+
+const addTask=(titel,descrption,duedate)=>{
+    const task={
+        titel:titel,
+        descrption:descrption,
+        duedate:duedate,
+        completed:false
+
+    }
+    tasks.push(task);
+}
+//calling addTask function
+addTask('leisure','trip to musanze','2023-12-9');
+addTask('meeting','school parents meeting','2024-2-13');
+addTask('study','learning algorithm',Date());
+console.log(tasks);
+//function to display tasks sorted by duedates
+const displaytasks=()=>{
+    let sortedTasks=tasks.sort((a,b)=>new Date(a.duedate)-new Date(b.duedate));
+    console.log(sortedTasks);
+}
+//calling displaytasks function
+console.log('Scheduler tasks:');
+displaytasks();
+//function to update task
+const update=(index,newtitel,newdescrption,newduedate,iscomplete)=>{
+    if(index>=0 && index<tasks.length){
+        tasks[index].titel=newtitel;
+        tasks[index].descrption=newdescrption;
+        tasks[index].duedate=newduedate;
+        tasks[index].completed=iscomplete;}
+        else {
+            console.log('task does not exist!');
+        }
+    }
+
+//updating task
+update(1,'Learn','learning node js','2024-3-11',false);
+console.log(tasks);
+//function to remove a task
+const removetask=(index)=>{
+    if (index>=0&&index<=tasks.length){
+        tasks.splice(index,1);
+    }
+    else{
+        console.log('task does not exist!');
+    }
+
+} 
+//removing a task
+
+console.log(tasks);
